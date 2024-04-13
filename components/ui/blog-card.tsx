f6b205a04f33174f9bd9bec71cc499243b01bef2
Ioannis Chrysostomakis <ic768@protonmail.com>
2024-04-13 21:04:28 +0300

    framer motion

--- components/ui/blog-card.tsx
+++ components/ui/blog-card.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { motion } from "framer-motion";
 import Image from "next/image";
 
 import {
@@ -9,21 +11,32 @@ import {
 import { Blog } from "@/lib/definitions";
 
 export default function BlogCard({ title, author, date, image }: Blog) {
+  const arrow = {
+    initial: { scale: 1, transition: { duration: 0.3 } },
+    animate: { scale: 1.12, transition: { duration: 0.3 } },
+  };
+
   return (
-    <Card className="h-[29rem] w-[27rem]">
-      <CardHeader className="h-full">
-        <Image
-          src={image}
-          width={350}
-          height={350}
-          alt="Picture of the author"
-        />
-        <CardTitle className="line-clamp-2 text-xl">{title}</CardTitle>
-        <CardDescription className="!mt-auto flex flex-col">
-          <span className="text-base">{author}</span>
-          <span>{date.toDateString()}</span>
-        </CardDescription>
-      </CardHeader>
-    </Card>
+    <motion.div initial="initial" animate="initial" whileHover="animate">
+      <Card className="h-[29rem] w-[27rem]">
+        <CardHeader className="h-full">
+          <div className="overflow-hidden">
+            <motion.div variants={arrow}>
+              <Image
+                src={image}
+                width={350}
+                height={350}
+                alt="Picture of the author"
+              />
+            </motion.div>
+          </div>
+          <CardTitle className="line-clamp-2 text-xl">{title}</CardTitle>
+          <CardDescription className="!mt-auto flex flex-col">
+            <span className="text-base">{author}</span>
+            <span>{date.toDateString()}</span>
+          </CardDescription>
+        </CardHeader>
+      </Card>
+    </motion.div>
   );
 }
