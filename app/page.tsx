e98749021047fb4d72c799debc4535112dbb8a28
Ioannis Chrysostomakis <ic768@protonmail.com>
2024-05-05 00:33:10 +0300

    Sign in works

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-
+import { SignInButton } from "./_components/sign-in-button";
 import SparkleSection from "./_components/sparkle-section";
 import TextSection from "./_components/text-section";
 
@@ -7,7 +7,9 @@ import Header from "@/components/ui/header";
 export default function LandingPage() {
   return (
     <>
-      <Header/>
+      <Header>
+        <SignInButton />
+      </Header>
       <SparkleSection />
       <div className="flex min-h-full flex-col p-8">
         <TextSection />
---
+++ auth.ts
@@ -0,0 +1,48 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Credentials({
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (_credentials) => {
+        let user = null;
+
+        // logic to salt and hash password
+        //const pwHash = saltAndHashPassword(credentials.password);
+
+        // logic to verify if user exists
+        //user = await getUserFromDb(credentials.email, pwHash);
+
+        user = { id: "test", email: "test@test.test" };
+
+        if (!user) {
+          // No user found, so this is their first attempt to login
+          // meaning this is also the place you could do registration
+          throw new Error("User not found.");
+        }
+
+        // return user object with the their profile data
+        return user;
+      },
+    }),
+  ],
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL("/dashboard", nextUrl));
+      }
+      return true;
+    },
+  },
+});
