9cf35d4003e13db741d8feda22043aaa5d3b960d
Ioannis Chrysostomakis <ic768@protonmail.com>
2024-05-05 00:42:33 +0300

    Add signout button

--- app/(authenticated)/layout.tsx
+++ app/(authenticated)/layout.tsx
@@ -2,15 +2,16 @@ import Image from "next/image";
 import Link from "next/link";
 
 import logoImage from "../../public/logo.jpg";
+import { SignOutButton } from "../_components/sign-out-button";
 
-import ThemeSwitcher from "@/components/theme-switcher";
+import Header from "@/components/ui/header";
 
 export default function Layout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <>
-      <header className="sticky top-0 z-10 flex justify-between gap-4 border-b border-zinc-300/25 bg-white/75 p-6 backdrop-blur-md dark:border-zinc-800/50 dark:bg-zinc-900/75 dark:text-white">
+      <Header>
         <Link href="/">
           <Image
             className="w-20 rounded-md dark:outline dark:outline-1 dark:outline-white"
@@ -18,8 +19,8 @@ export default function Layout({
             alt="logo"
           />
         </Link>
-        <ThemeSwitcher />
-      </header>
+        <SignOutButton />
+      </Header>
       <div className="flex min-h-full flex-col p-8">{children}</div>
     </>
   );
