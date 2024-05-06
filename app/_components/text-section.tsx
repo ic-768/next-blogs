ebada1c349f9015a4e36347dddac361e440b1446
Ioannis Chrysostomakis <ic768@protonmail.com>
2024-05-06 15:31:45 +0300

    Next Blogs to NextBlogs

--- app/_components/text-section.tsx
+++ app/_components/text-section.tsx
@@ -10,20 +10,20 @@ export default function TextSection() {
   return (
     <div className="grid grid-cols-1 gap-16 pt-8 md:grid-cols-2 lg:grid-cols-3">
       <TextCard title="Empower Your Creativity" image={Landing1}>
-        Unlock your potential as a content creator with Next Blogs. Whether
+        Unlock your potential as a content creator with NextBlogs. Whether
         you&apos;re a seasoned blogger or just starting out, our intuitive
         platform provides everything you need to bring your ideas to life. From
-        sleek designs to powerful editing tools, Blogify empowers you to express
-        yourself with ease.
+        sleek designs to powerful editing tools, NextBlogs empowers you to
+        express yourself with ease.
       </TextCard>
       <TextCard title="Effortless Design" image={Landing2}>
-        Say goodbye to complicated coding and design headaches. With Next Blogs,
+        Say goodbye to complicated coding and design headaches. With NextBlogs,
         crafting a stunning blog is as simple as can be. Captivate your audience
         and enhance your content. Stand out from the crowd with
         professional-looking blogs that reflect your unique style.
       </TextCard>
       <TextCard title="Seamless Publishing" image={Landing3}>
-        Ready to share your thoughts with the world? With Next Blogs, publishing
+        Ready to share your thoughts with the world? With NextBlogs, publishing
         your content is a breeze. Our streamlined process ensures that your
         posts go live with just a few clicks.Reach your audience whenever and
         wherever they are.
@@ -35,9 +35,9 @@ export default function TextSection() {
       </TextCard>
       <TextCard title="Start Your Blogging Journey Today" image={Landing5}>
         Ready to take the first step towards becoming a successful blogger? Sign
-        up for Next Blogs now and embark on your blogging journey with
+        up for NextBlogs now and embark on your blogging journey with
         confidence. Whether you&apos;re blogging for fun or aiming to turn your
-        passion into a career, Next Blogs is the perfect platform to help you
+        passion into a career, NextBlogs is the perfect platform to help you
         achieve your goals.
       </TextCard>
     </div>
