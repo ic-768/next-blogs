ce81d6c0b2a9479aac3625e2ec637e3dc4c810f1
Ioannis Chrysostomakis <ic768@protonmail.com>
2024-05-06 13:23:50 +0300

    Login form

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,5 +1,89 @@
-import SignInForm from "./form";
+"use client";
 
-export default function SignIn() {
-  return <SignInForm />;
+import { FormEventHandler, useRef } from "react";
+
+import { signIn } from "next-auth/react";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { cn } from "@/lib/utils";
+
+export default function SignInForm() {
+  const eRef = useRef<HTMLInputElement>(null);
+  const pRef = useRef<HTMLInputElement>(null);
+
+  const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    signIn("credentials", {
+      email: eRef.current?.value,
+      password: pRef.current?.value,
+    });
+  };
+
+  return (
+    <div className="flex h-screen flex-col justify-center">
+      <div className="mx-auto w-full max-w-md rounded bg-white p-4 shadow dark:bg-black md:rounded-2xl md:p-8">
+        <h2 className="text-xl font-bold text-neutral-800 dark:text-neutral-200">
+          Welcome to NextBlogs
+        </h2>
+        <p className="mt-2 max-w-sm text-sm text-neutral-600 dark:text-neutral-300">
+          Use any credentials you want because I want to focus on the frontend
+          :)
+        </p>
+
+        <form className="my-8" onSubmit={onSubmit}>
+          <LabelInputContainer className="mb-4">
+            <Label htmlFor="email">Email Address</Label>
+            <Input
+              ref={eRef}
+              id="email"
+              placeholder="email@example.com"
+              type="email"
+            />
+          </LabelInputContainer>
+          <LabelInputContainer className="mb-4">
+            <Label htmlFor="password">Password</Label>
+            <Input
+              ref={pRef}
+              id="password"
+              placeholder="••••••••"
+              type="password"
+            />
+          </LabelInputContainer>
+
+          <Button
+            className="group/btn relative block h-10 w-full rounded-md bg-gradient-to-br from-black to-neutral-600 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:bg-zinc-800 dark:from-zinc-900 dark:to-zinc-900 dark:text-white dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+            type="submit"
+          >
+            Sign up &rarr;
+            <BottomGradient />
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
 }
+
+const BottomGradient = () => {
+  return (
+    <>
+      <span className="absolute inset-x-0 -bottom-px block h-px w-full bg-gradient-to-r from-transparent via-cyan-500 to-transparent opacity-0 transition duration-500 group-hover/btn:opacity-100" />
+      <span className="absolute inset-x-10 -bottom-px mx-auto block h-px w-1/2 bg-gradient-to-r from-transparent via-indigo-500 to-transparent opacity-0 blur-sm transition duration-500 group-hover/btn:opacity-100" />
+    </>
+  );
+};
+
+const LabelInputContainer = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div className={cn("flex w-full flex-col space-y-2", className)}>
+      {children}
+    </div>
+  );
+};
