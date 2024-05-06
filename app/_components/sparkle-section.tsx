860a8fb4119b22ba3174fc79af82255338acefea
Ioannis Chrysostomakis <ic768@protonmail.com>
2024-05-06 15:11:36 +0300

    Handle sparkle theme change

--- app/_components/sparkle-section.tsx
+++ app/_components/sparkle-section.tsx
@@ -1,12 +1,16 @@
 "use client";
 
+import { useTheme } from "next-themes";
+
 import { SparklesCore } from "@/components/sparkles";
 
 export default function SparkleSection() {
+  const { resolvedTheme } = useTheme();
+
   return (
     <div className="flex h-[20rem] w-full flex-col items-center justify-center overflow-hidden rounded md:h-[40rem]">
-      <h1 className="relative text-center text-3xl font-bold text-white md:text-7xl lg:text-9xl">
-        Next Blogs
+      <h1 className="relative text-center text-3xl font-bold dark:text-white md:text-7xl lg:text-9xl">
+        NextBlogs
       </h1>
       <div className="relative mt-4 h-40 w-[40rem] sm:w-4/5">
         {/* Gradients */}
@@ -16,12 +20,13 @@ export default function SparkleSection() {
         <div className="absolute inset-x-60 top-0 h-px w-1/4 bg-gradient-to-r from-transparent via-sky-500 to-transparent" />
 
         <SparklesCore
+          key={resolvedTheme}
           background="transparent"
           minSize={0.4}
           maxSize={1}
           particleDensity={1200}
           className="h-full w-full"
-          particleColor="#FFFFFF"
+          particleColor={resolvedTheme === "dark" ? "FFFFFF" : "000000"}
         />
 
         {/* Radial Gradient to prevent sharp edges */}
